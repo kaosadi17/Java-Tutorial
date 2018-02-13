aae6f472ce48f00768cff1fdfe8d867ddf0bf2ea
amalio <kaosadi17@gmail.com>
2018-02-13 14:38:17 +0530

    added Java scanner more

--- JavaTute/src/UserInput.java
+++ JavaTute/src/UserInput.java
@@ -7,6 +7,15 @@ public class UserInput {
 		Scanner scan =  new Scanner(System.in);
 		System.out.println(scan.nextLine());
 		
+		int num1,num2,num3;
+		System.out.println("Enter the first number");
+		num1 = scan.nextInt();
+		System.out.println("Enter the second number");
+		num2 = scan.nextInt();
+		num3 = num1 + num2;
+		System.out.println(num3);
+		
 	}
 
+	
 }
