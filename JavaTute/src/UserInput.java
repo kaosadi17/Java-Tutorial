a538d974f15bae7d0c1fa3dad55cbd95722932f6
amalio <kaosadi17@gmail.com>
2018-02-13 13:33:45 +0530

    added Java User input

---
+++ JavaTute/src/UserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UserInput {
+	
+	public static void main(String args[]){
+		System.out.println("Enter Your Name");
+		Scanner scan =  new Scanner(System.in);
+		System.out.println(scan.nextLine());
+		
+	}
+
+}
