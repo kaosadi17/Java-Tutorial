471f260301ed78dc9691b44816a14f16080d7ce8
amalio <kaosadi17@gmail.com>
2018-02-13 11:32:11 +0530

    added Java Variables

--- JavaTute/src/HelloWorld.java
+++ JavaTute/src/HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
 	
 	public static void main(String args[]){
 		System.out.println("Hello World");
+		
+		double apples; //Declaring variable
+		apples = 3.14; //assigning variable
+		System.out.println(apples);
 	}
 
 }
