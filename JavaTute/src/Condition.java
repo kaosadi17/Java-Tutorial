f6d53c3e95846ced4612d3bb62fc610b9485960c
amalio <kaosadi17@gmail.com>
2018-02-14 13:17:29 +0530

    added Java conditions

---
+++ JavaTute/src/Condition.java
@@ -0,0 +1,19 @@
+
+public class Condition {
+
+	public static void main(String[] args) {
+		
+		int test = 10;
+		
+		if(test <20){
+			System.out.println("if condition passed");
+		}
+		
+		else{
+			System.out.println("else condition passed");
+		}
+		
+
+	}
+
+}
